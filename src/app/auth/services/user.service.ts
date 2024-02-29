d2e9a0d027420e34dfb9441a7ec928b63f0ccf00
andres.mera <feli041089@gmail.com>
2024-02-29 09:59:59 -0500

    feature/authentication : Creando el servicio para obtener el token.

--- src/app/auth/services/user.service.ts
+++ src/app/auth/services/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import { environments } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
-import { User } from '../interfaces/user.interface';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { environments } from '../../../environments/environments';
+
+import { User } from '../interfaces/user.interface';
 import { Message } from '../interfaces/message.interface';
+import { Token } from '../interfaces/token.interface'
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +19,9 @@ export class UserService {
 
   public saveUser(user: User): Observable<Message> {
     return this.httpClient.post<Message>(`${this.baseUrl}/signup`, user);
+  }
+
+  public postTokenUser(user: User): Observable<Token> {
+    return this.httpClient.post<Token>(`${this.baseUrl}/login`, user);
   }
 }
