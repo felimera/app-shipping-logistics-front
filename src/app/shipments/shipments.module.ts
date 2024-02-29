c4f01cebbec362383d802fa346f765652c5cfbf9
andres.mera <feli041089@gmail.com>
2024-02-29 10:51:00 -0500

    feature/shipments : Creacion de los los componentes de layout, register y search.

--- src/app/shipments/shipments.module.ts
+++ src/app/shipments/shipments.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from '../material/material.module';
 
 import { ShipmentsRoutingModule } from './shipments-routing.module';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 
 @NgModule({
   declarations: [
+    SearchPageComponent,
+    RegisterPageComponent,
+    LayoutPageComponent
   ],
   imports: [
-    CommonModule,
-    ShipmentsRoutingModule
+    ShipmentsRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
   ]
 })
 export class ShipmentsModule { }
