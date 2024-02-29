fad2d89ad56bd04a71cbb83a8484bd016d82eb52
andres.mera <feli041089@gmail.com>
2024-02-29 08:16:46 -0500

    feature/authentication : Agregando modulo reactivo para el formulario.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
@@ -19,6 +20,7 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
     CommonModule,
     AuthRoutingModule,
     MaterialModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
