55bdce743125303e4da0e59b28a0ae761b7c259a
andres.mera <feli041089@gmail.com>
2024-02-29 15:57:21 -0500

    feature/shipments : Modificacion del componente de buscar vehiculo.

--- src/app/shared/components/vehicle-search/vehicle-search.component.ts
+++ src/app/shared/components/vehicle-search/vehicle-search.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+import { Vehicle } from '../../../vehicle/interfaces/vehicle.interface';
+import { VehicleSearchService } from '../../services/vehicle-search.service';
 
 @Component({
   selector: 'shared-vehicle-search',
@@ -7,4 +12,28 @@ import { Component } from '@angular/core';
 })
 export class VehicleSearchComponent {
 
+  public searchInput = new FormControl('');
+  public vehicles: Vehicle[] = [];
+  public selectedVehicle?: Vehicle;
+
+  constructor(private vehicleSeachService: VehicleSearchService) { }
+
+  public searchVehicle(): void {
+    const value: string = this.searchInput.value || '';
+
+    this.vehicleSeachService
+      .getVehicleList(value)
+      .subscribe(vehicle => this.vehicles = vehicle);
+  }
+
+  public onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+    if (!event.option.value) {
+      this.selectedVehicle = undefined;
+      return;
+    }
+    const vehicle: Vehicle = event.option.value;
+    this.searchInput.setValue(vehicle.licensePlate);
+
+    this.selectedVehicle = vehicle;
+  }
 }
