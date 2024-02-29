a12e4bdb0fe6cdd52fddfec3ad11b5f7dd4a783a
andres.mera <feli041089@gmail.com>
2024-02-29 10:57:41 -0500

    feature/shipments : Agregando las rutas de la aplicacion.

--- src/app/shipments/shipments-routing.module.ts
+++ src/app/shipments/shipments-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'new-register', component: RegisterPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: '**', redirectTo: 'new-register' },
+    ]
+  }
 ];
 
 @NgModule({
