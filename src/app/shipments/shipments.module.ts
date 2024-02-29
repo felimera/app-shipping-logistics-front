e4f79006c0a5227e23cfea0fcb8973a8e966ec85
andres.mera <feli041089@gmail.com>
2024-02-29 10:45:20 -0500

    feature/shipments : Retiro de los los compinetes de register y search

--- src/app/shipments/shipments.module.ts
+++ src/app/shipments/shipments.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ShipmentsRoutingModule } from './shipments-routing.module';
-import { RegisterComponent } from './pages/register/register.component';
-import { SearchComponent } from './pages/search/search.component';
 
 
 @NgModule({
   declarations: [
-    RegisterComponent,
-    SearchComponent
   ],
   imports: [
     CommonModule,
