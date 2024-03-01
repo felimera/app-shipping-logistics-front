61c54856a47212285de4cd02008a831e82f9b9cd
andres.mera <feli041089@gmail.com>
2024-03-01 07:56:28 -0500

    feature/shipments : Creacion del componente del busque de los barcos.

--- src/app/shared/components/ship-search/ship-search.component.ts
+++ src/app/shared/components/ship-search/ship-search.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+import { Shipments } from '../../../shipments/interfaces/shipment.interface';
+import { ShipSearchService } from '../../services/ship-search.service';
 
 @Component({
   selector: 'shared-ship-search',
@@ -7,4 +12,28 @@ import { Component } from '@angular/core';
 })
 export class ShipSearchComponent {
 
+  public searchInput = new FormControl('');
+  public shipments: Shipments[] = [];
+  public selectedShip?: Shipments;
+
+  constructor(private shipSearchService: ShipSearchService) { }
+
+  public searchShip(): void {
+    const value: string = this.searchInput.value || '';
+
+    this.shipSearchService
+      .getShipList(value)
+      .subscribe(shipments => this.shipments = shipments);
+  }
+
+  public onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+    if (!event.option.value) {
+      this.selectedShip = undefined;
+      return;
+    }
+    const shipment: Shipments = event.option.value;
+    this.searchInput.setValue(shipment.fleetNumber);
+
+    this.selectedShip = shipment;
+  }
 }
