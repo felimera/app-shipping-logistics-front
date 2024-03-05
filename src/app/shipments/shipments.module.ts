b79994845f541032c8e5b93470ea3ab2167ed343
andres.mera <feli041089@gmail.com>
2024-03-05 16:18:59 -0500

    feature/shipments : Desarrollo completo para la consulta y registro de los datos del envio terestre o marino.

--- src/app/shipments/shipments.module.ts
+++ src/app/shipments/shipments.module.ts
@@ -11,13 +11,21 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 import { SharedModule } from '../shared/shared.module';
 import { ProductModule } from '../product/product.module';
+import { ShipmentPriceSearchComponent } from './components/shipment-price-search/shipment-price-search.component';
+import { ShipmentAmountSearchComponent } from './components/shipment-amount-search/shipment-amount-search.component';
+import { ShipmentDiscountSearchComponent } from './components/shipment-discount-search/shipment-discount-search.component';
+import { ShipmentListComponent } from './components/shipment-list/shipment-list.component';
 
 
 @NgModule({
   declarations: [
     SearchPageComponent,
     RegisterPageComponent,
-    LayoutPageComponent
+    LayoutPageComponent,
+    ShipmentPriceSearchComponent,
+    ShipmentAmountSearchComponent,
+    ShipmentDiscountSearchComponent,
+    ShipmentListComponent
   ],
   imports: [
     CommonModule,
